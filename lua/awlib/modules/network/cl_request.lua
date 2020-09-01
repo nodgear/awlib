02187c84c6a7455151a64cd76b868c5dd4fafe6e
ceifa <gabrinelson27@gmail.com>
2020-09-01 19:42:57 -0300

    Fix network request collisions

--- lua/awlib/modules/network/cl_request.lua
+++ lua/awlib/modules/network/cl_request.lua
@@ -1,20 +1,25 @@
 local promises = {}
 
+local function buildRequestIdentifier(identifier)
+    return string.format("%s:%s", math.random(1, 1000000), identifier)
+end
+
 function Aw:ServerRequest(sIdentifier, fCallback, ...)
-    promises[sIdentifier] = fCallback
+    local requestIdentifier = buildRequestIdentifier(sIdentifier)
+    promises[requestIdentifier] = fCallback
 
     net.Start("AW.ClientRequest")
-        net.WriteString(sIdentifier)
+        net.WriteString(requestIdentifier)
         net.WriteTable({...})
     net.SendToServer()
 end
 
 net.Receive("AW.ServerResponse", function(len)
-    local identifier = net.ReadString()
+    local requestIdentifier = net.ReadString()
     local response = net.ReadType()
 
-    if promises[identifier] then
-        promises[identifier](response)
-        promises[identifier] = nil
+    if promises[requestIdentifier] then
+        promises[requestIdentifier](response)
+        promises[requestIdentifier] = nil
     end
 end)
--- lua/awlib/modules/network/sv_request.lua
+++ lua/awlib/modules/network/sv_request.lua
@@ -1,13 +1,20 @@
+local function getRequestIdentifier(builtIdentifier)
+    local separatorIdx = string.find(builtIdentifier, ":")
+    return string.sub(builtIdentifier, separatorIdx + 1)
+end
+
 util.AddNetworkString("AW.ClientRequest")
 util.AddNetworkString("AW.ServerResponse")
 
 net.Receive("AW.ClientRequest", function(len, ply)
-    local identifier = net.ReadString()
+    local requestIdentifier = net.ReadString()
     local arguments = net.ReadTable()
+
+    local identifier = getRequestIdentifier(requestIdentifier)
 
     hook.Run(identifier, ply, function(response)
         net.Start("AW.ServerResponse")
-            net.WriteString(identifier)
+            net.WriteString(requestIdentifier)
             net.WriteType(response)
         net.Send(ply)
     end, unpack(arguments))
