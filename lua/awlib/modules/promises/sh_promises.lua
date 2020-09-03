c789db73c7d96d7863ad22744ad15585c18012d3
Nodge <matheus.silva3@ftc.edu.br>
2020-09-03 05:47:42 -0300

    Licenses

--- lua/awlib/modules/promises/sh_promises.lua
+++ lua/awlib/modules/promises/sh_promises.lua
@@ -1,8 +1,21 @@
--- FIXME: Move promises to promise module and namespacing
--- https://github.com/zserge/lua-promises/blob/master/deferred.lua
+--[[
+	LICENSE:
+	Definitions:
+		the Xenin/XeninUI Copyright Owner (hereby Patrick Ratzow) under Custom Software License
+		the Awesome Copyright Owner (hereby Matheus A.) under Custom Software License
+		the XLib Copyright Owner (hereby Xavier B.) under MIT License
+		the Software (hereby the Library and it's contents)
+		Garry's Mod Marketplace is any online and offline marketplace that sells Garry's Mod game modifications in any way, including, but not limited to Gmodstore.com
 
---- A+ promises in Lua.
---- @module deferred
+	Ownership:
+		the use of this library is intended, but not limited to the use by Awesome Copyright owner on Garry's Mod Marketplace
+		modifying, selling or sharing this piece of software is not allowed unless respecting all above licenses
+
+]]--
+
+-- Module: Promises/A+ standard
+-- Author:
+--      Brian Hang
 
 local M = {}
 
@@ -333,4 +346,4 @@ end
 --- @param value promise value
 --- @return rejected future result
 
-Aw.UI.Promises = M
+Aw.Promises = M
