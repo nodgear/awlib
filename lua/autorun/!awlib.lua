468939f91c5d0d33ff73c7539f018654a416af6f
Nodge <matheus.silva3@ftc.edu.br>
2020-09-12 02:46:47 -0300

    Added load hook to awlib

--- lua/autorun/!awlib.lua
+++ lua/autorun/!awlib.lua
@@ -4,4 +4,14 @@ _MODULES.awlib = true
 require "xloader"
 
 Aw = Aw or {}
-xloader("awlib", function(f) include(f) end)
+xloader("awlib", function(f)
+    if (timer.Exists("_load.awLib")) then
+        timer.Remove("_load.awLib")
+    end
+
+    timer.Create("_load.awLib", wait, 1, function()
+        hook.Run("AWESOME.LIB.Hook.Loaded")
+        timer.Remove("_load.awLib")
+    end)
+    include(f)
+end)
