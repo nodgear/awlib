920eae00d6559fc6152eaa6fa03debdafca17a2c
Nodge <matheus.silva3@ftc.edu.br>
2020-09-12 02:39:46 -0300

    Moved easing curve functions

--- lua/awlib/modules/ui_utils/cl_essentials.lua
+++ lua/awlib/modules/ui_utils/cl_essentials.lua
@@ -270,3 +270,21 @@ function Aw.UI:BuildButtonSizes(pnl)
         end
     end
 end
+
+function Aw.UI:Ease(t, b, c, d)
+	t = t / d
+	local ts = t * t
+	local tc = ts * t
+
+	--return b + c * ts
+	return b + c * (-2 * tc + 3 * ts)
+end
+
+function Aw.UI:EaseInOutQuintic(t, b, c, d)
+	t = t / d
+	local ts = t * t
+	local tc = ts * t
+
+	--return b + c * ts
+	return b + c * (6 * tc * ts + -15 * ts * ts + 10 * tc)
+end
