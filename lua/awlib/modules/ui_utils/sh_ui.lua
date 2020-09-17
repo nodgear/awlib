7e1ac53643b9cbc5c4c37a60c517a3da9906997a
Nodge <Matheus.silva3@ftc.edu.br>
2020-09-17 18:56:42 -0300

    Added emotes

--- lua/awlib/modules/ui_utils/sh_ui.lua
+++ lua/awlib/modules/ui_utils/sh_ui.lua
@@ -1,2 +1,21 @@
-Aw.UI = {}
+Aw.UI = Aw.UI or {}
 Aw.UI.TransitionTime = .3
+
+
+Aw.UI.Emoticons = {}
+
+Aw.UI.Emoticons[":)"] = "icon16/emoticon_smile.png"
+Aw.UI.Emoticons[":D"] = "icon16/emoticon_happy.png"
+Aw.UI.Emoticons[":O"] = "icon16/emoticon_surprised.png"
+Aw.UI.Emoticons[":p"] = "icon16/emoticon_tongue.png"
+Aw.UI.Emoticons[":P"] = "icon16/emoticon_tongue.png"
+Aw.UI.Emoticons[":("] = "icon16/emoticon_unhappy.png"
+
+Aw.UI.Emoticons[":smile:"] = "icon16/emoticon_smile.png"
+Aw.UI.Emoticons[":online:"] = "icon16/status_online.png"
+Aw.UI.Emoticons[":tongue:"] = "icon16/emoticon_tongue.png"
+Aw.UI.Emoticons[":offline:"] = "icon16/status_offline.png"
+Aw.UI.Emoticons[":unhappy:"] = "icon16/emoticon_unhappy.png"
+Aw.UI.Emoticons[":suprised:"] = "icon16/emoticon_surprised.png"
+Aw.UI.Emoticons[":exclamation:"] = "icon16/exclamation.png"
+Aw.UI.Emoticons[":information:"] = "icon16/information.png"
