09dc142a1d9ceee3269b43da5ede0f1eb856dd99
Nodge <Matheus.silva3@ftc.edu.br>
2020-09-25 12:29:09 -0300

    Added inbound and stack to panel meta tables

--- lua/awlib/modules/ui_utils/cl_panelhook.lua
+++ lua/awlib/modules/ui_utils/cl_panelhook.lua
@@ -1,6 +1,33 @@
-if !Aw.UI.__AddedPanelFunctions then
+-- if !Aw.UI.__AddedPanelFunctions then
 	local PNL = FindMetaTable("Panel")
 	local Old_Remove = Old_Remove or PNL.Remove
+
+	function PNL:Stack(identifier)
+		Aw.UI.PanelStack = Aw.UI.PanelStack or {}
+		Aw.UI.PanelStack[identifier] = self
+
+		self.StackID = identifier
+		self.StackNumber = table.Count(Aw.UI.PanelStack)
+	end
+
+	function PNL:GetStack(w,h,m)
+		if !self.StackID then return end
+		return self.StackNumber, self.StackNumber * w + m * self.StackNumber, self.StackNumber * h + m * self.StackNumber -- this will return the stack number,
+	end
+
+	function PNL:RemoveStack()
+		if !self.StackID then return end
+		Aw.UI.PanelStack[self.StackID] = nil
+		hook.Run("StackValueChange", self.StackNumber)
+	end
+
+	function PNL:InBound()
+		local w, h = self:GetWide(), self:GetTall()
+		local px, py = self:LocalToScreen()
+		local mx, my = gui.MousePos()
+		local tx, ty = math.Clamp(mx - px, -1, w + 1), math.Clamp(my - py, -1, h + 1)
+		return  (tx < w and tx >= 0) and (ty < h + 1 and ty >= 0)
+	end
 
 	function PNL:Remove()
 		for k, v in pairs( self.hooks or {} ) do
@@ -9,6 +36,10 @@ if !Aw.UI.__AddedPanelFunctions then
 
 		for k, v in pairs( self.timers or {} ) do
 			timer.Remove( k )
+		end
+
+		if self.StackID then
+			self:RemoveStack()
 		end
 
 		Old_Remove( self )
@@ -67,5 +98,5 @@ if !Aw.UI.__AddedPanelFunctions then
 		end)
 	end
 
-	Aw.UI.__AddedPanelFunctions = true
-end
+-- 	Aw.UI.__AddedPanelFunctions = true
+-- end
