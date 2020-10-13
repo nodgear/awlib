cdaa338bc56eb75ca120b3f2e296a53d98583c6a
Nodge <Matheus.silva3@ftc.edu.br>
2020-10-13 11:37:22 -0300

    Added Panel:DrawGrid

--- lua/awlib/modules/ui_utils/cl_panelhook.lua
+++ lua/awlib/modules/ui_utils/cl_panelhook.lua
@@ -21,6 +21,60 @@
 		hook.Run("StackValueChange", self.StackNumber)
 	end
 
+	function PNL:DrawGrid( material, pixelGridUnits, textureGridDivisions )
+		self.BG = Material(material .. "_fixed")
+		local strImage = self.BG:GetName()
+	
+		if self.BG:IsError() then
+			self.BG = CreateMaterial(material .. "_fixed", "VertexLitGeneric", "")
+			self.BG:SetTexture("$basetexture", Material(material):GetTexture("$basetexture"))
+		end
+	
+		local Mat = self.BG
+	
+		if ( string.find( Mat:GetShader(), "VertexLitGeneric" ) || string.find( Mat:GetShader(), "Cable" ) ) then
+	
+			local t = Mat:GetString( "$basetexture" )
+	
+			if ( t ) then
+	
+				local params = {}
+				params[ "$basetexture" ] = t
+				params[ "$vertexcolor" ] = 1
+				params[ "$vertexalpha" ] = 1
+	
+				Mat = CreateMaterial( strImage .. "_DImage", "UnlitGeneric", params )
+	
+			end
+	
+		end
+	
+		self.BG = Mat
+
+
+		local size = self:GetSize()
+		local texture = self.BG:GetTexture("$basetexture")
+		local tw = texture:GetMappingWidth()
+		local th = texture:GetMappingHeight()
+			
+		local scale = (tw/pixelGridUnits) / textureGridDivisions
+	
+		local u0, v0 = 0,0
+		local u1, v1 = (size*2 / tw) * scale, (size*2 / th) * scale
+	
+		u0 = u0 - (u1 % 1)
+		v0 = v0 - (v1 % 1)
+	
+		local du = 0.5 / tw
+		local dv = 0.5 / th
+		u0, v0 = ( u0 - du ) / ( 1 - 2 * du ), ( v0 - dv ) / ( 1 - 2 * dv )
+		u1, v1 = ( u1 - du ) / ( 1 - 2 * du ), ( v1 - dv ) / ( 1 - 2 * dv )
+	
+		surface.SetMaterial(self.BG)
+		surface.DrawTexturedRectUV( -size, -size, size*2, size*2, u0, v0, u1, v1 )
+	
+	end
+
 	function PNL:InBound()
 		local w, h = self:GetWide(), self:GetTall()
 		local px, py = self:LocalToScreen()
